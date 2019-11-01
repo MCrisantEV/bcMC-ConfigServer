eeddc9784764bb70ed6a5ce0c8430b60005de4ba
MCrisantEV <mcrisant@everis.com>
2019-11-01 11:10:36 -0500

    Configuracion Inicial del ConfigServer

---
+++ README.md
@@ -0,0 +1,10 @@
+# bcMC-Configuration
+
+Config Server, centraliza y externaliza las configuraciones de los MS
+
+Configuración de las propiedades de los MS 
+- MS Course: Port: 8902
+- MS Family: Port: 8906
+- MS Inscription: Port: 8903
+- MS Institute: Port: 8901
+- MS Person: Port: 8904
---
+++ src/test/java/mc/bc/config/server/app/BcMcConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package mc.bc.config.server.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BcMcConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
